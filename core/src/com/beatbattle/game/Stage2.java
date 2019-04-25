b10d1b5d0038f103ec0a36c3ca346763d460f93f
Riley Bowling <ostrichking7@gmail.com>
2019-04-25 18:16:16 -0500

    Revert "added start menu and nackground"
    This reverts commit b12ba2acf61a4a974fd16256ad8545dd77e52e1d.

--- core/src/com/beatbattle/game/Stage2.java
+++ core/src/com/beatbattle/game/Stage2.java
@@ -1,21 +1,14 @@
 package com.beatbattle.game;
 
 import com.badlogic.gdx.Gdx;
- 
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
- 
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
- 
 import com.badlogic.gdx.graphics.Texture;
- 
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
- 
- 
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.beatbattle.game.BeatBattle;
- 
 
 public class Stage2 implements Screen {
 
@@ -25,29 +18,16 @@ public class Stage2 implements Screen {
     ExtendViewport viewport;
     WriteTrack track;
     BasicBeat beat;
-    
-    //variables for the background
-    Texture texture;
-    SpriteBatch batch;
-    TextureRegion region;
-    
-  
 
     public Stage2(final BeatBattle game) {
         this.game = game;
-        
 
         camera = new OrthographicCamera();
         viewport = new ExtendViewport(800, 600, camera);
         viewport.apply();
-        track = new WriteTrack((int) viewport.getMinWorldWidth()/2);
+        track = new WriteTrack((int) viewport.getMinWorldWidth()/2, 100);
         beat = new BasicBeat(50,50);
-        
-        texture = new Texture( Gdx.files.internal("bg.jpg") );
-        batch = new SpriteBatch();
-        region = new TextureRegion( texture );
-        
-   }
+    }
 
     @Override
     public void render(float delta) {
@@ -60,13 +40,11 @@ public class Stage2 implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         game.batch.setProjectionMatrix(camera.combined);
         game.batch.begin();
-       // game.batch.draw( texture, 50, 50 );
-        game.batch.draw(texture,0,0);
 
         track.run(game.batch, 100, delta);
+
         game.batch.end();
-        
-       
+
     }
 
     @Override
